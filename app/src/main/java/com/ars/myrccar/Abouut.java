a05c46bd31bd68a270e007df5a095d46dfa2dd5a
Kelvin-M <flyboypac@gmail.com>
2017-02-16 12:31:13 +0300

    Initial commit

---
+++ app/src/main/java/com/ars/myrccar/Abouut.java
@@ -0,0 +1,18 @@
+//Application created by Arvind Sanjeev - DIYhacking.com
+package com.ars.myrccar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Abouut extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	setContentView(R.layout.abouut);
+	}
+	
+	
+
+}
